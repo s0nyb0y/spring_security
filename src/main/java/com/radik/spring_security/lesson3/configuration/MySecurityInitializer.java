95261faf4a6ee2171bc6ff08381b7fdac162a510
Radik Akhmetgareev <driveclub228@gmail.com>
2024-04-08 16:58:12 +0300

    lesson3 spring security авторизация + Database

---
+++ src/main/java/com/radik/spring_security/lesson3/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.radik.spring_security.lesson3.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
